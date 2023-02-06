6cab438534cf02297e724e22d86c8b215cd8e13a
Mario Samperi <mariosamperi@Marios-MacBook-Pro.local>
2023-02-06 13:21:07 +0100

    Creating Step Definitions and Running Feature Files

---
+++ src/test/java/tests/Login.java
@@ -0,0 +1,52 @@
+package tests;
+
+import io.cucumber.java.en.*;
+
+public class Login {
+	
+	@Given("User navigates to Login page")
+	public void user_navigates_to_login_page() {
+	    System.out.println(">> User got navigated to Login Page");
+	}
+
+	@When("User enters valid email address {string}")
+	public void user_enters_valid_email_address(String emailAddress) {
+		System.out.println(">> User entered valid email address: " + emailAddress);
+	}
+
+	@When("Enters valid password {string}")
+	public void enters_valid_password(String password) {
+		System.out.println(">> User entered valid password: " + password);
+	}
+
+	@When("Clicks on Login button")
+	public void clicks_on_login_button() {
+		System.out.println(">> User clicked on Login Button");
+	}
+
+	@Then("User should login successfully")
+	public void user_should_login_successfully() {
+		System.out.println(">> User got Logged In Successfully");
+	}
+
+	@When("User enters invalid email address {string}")
+	public void user_enters_invalid_email_address(String invalidEmailAddress) {
+		System.out.println(">> User entered invalid emailAddress: " + invalidEmailAddress);
+	}
+
+	@When("Enters invalid password {string}")
+	public void enters_invalid_password(String invalidPassword) {
+		System.out.println(">> User entered invalid password: " + invalidPassword);
+	}
+
+	@Then("User should get a proper warning message")
+	public void user_should_get_a_proper_warning_message() {
+		System.out.println(">> User got a proper warning message");
+	}
+
+	@When("User does not enter any credentials")
+	public void user_does_not_enter_any_credentials() {
+		System.out.println(">> User did not enter any credentials");
+	}
+
+}
