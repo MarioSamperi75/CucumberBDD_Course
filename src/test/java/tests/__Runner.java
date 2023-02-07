f186c1d1c4a57866b157f41647fb0f5eafd7d6b5
Mario Samperi <mariosamperi@marios-mbp.home>
2023-02-07 13:43:28 +0100

    HtmlReport

--- src/test/java/tests/__Runner.java
+++ src/test/java/tests/__Runner.java
@@ -3,8 +3,10 @@ package tests;
 import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"html:target/CucumberHTMLReport.html"})
 public class __Runner {
 
 }
