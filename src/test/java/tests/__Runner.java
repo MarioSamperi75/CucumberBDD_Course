81e3e2c01941e3f7bc72ec89f5da551b4b13fb03
Mario Samperi <mariosamperi@Marios-MacBook-Pro.local>
2023-02-08 13:48:43 +0100

    adding pretty plugin

--- src/test/java/tests/__Runner.java
+++ src/test/java/tests/__Runner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"html:target/CucumberHTMLReport.html"})
+@CucumberOptions(plugin = {"pretty","html:target/CucumberHTMLReport.html"})
 public class __Runner {
 
 }
