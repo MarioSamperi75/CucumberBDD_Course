f7d7bb744a9395a731c52d076d559f2121c76d7f
Mario Samperi <mariosamperi@Marios-MacBook-Pro.local>
2023-02-09 09:05:10 +0100

    order in multiple before

--- src/test/java/tests/__Hooks.java
+++ src/test/java/tests/__Hooks.java
@@ -6,37 +6,64 @@ import io.cucumber.java.Before;
 import io.cucumber.java.BeforeStep;
 
 public class __Hooks {
-	@Before
-	public void setup() {
-		System.out.println("Browser got opened");
+	
+	
+	
+	/*
+	 * @Before public void setup() { System.out.println("Browser got opened"); }
+	 * 
+	 * @After public void tearDown() { System.out.println("Browser got closed");
+	 * 
+	 * }
+	 * 
+	 * @Before(("@login")) public void onlyForBeforeLogin() {
+	 * System.out.println("onlyForBeforeLogin"); }
+	 * 
+	 * @After(("@login")) public void onlyForAfterLogin() {
+	 * System.out.println("onlyForAfterLogin");
+	 * 
+	 * }
+	 * 
+	 * @BeforeStep(("@register")) public void onlyForBeforeStepRegister() {
+	 * System.out.println("onlyForBeforeStepRegister"); }
+	 * 
+	 * @AfterStep(("@register")) public void onlyForAfterStepRegister() {
+	 * System.out.println("onlyForAfterStepRegister"); }
+	 */
+	
+	
+	
+	@Before(order=1)
+	public void before1() {
+		System.out.println("++ Starting before 1");
 	}
 	
-	@After
-	public void tearDown() {
-		System.out.println("Browser got closed");
-	
+	@Before(order=2)
+	public void before2() {
+		System.out.println("++ Starting before 2");
 	}
 	
-	@Before(("@login"))
-	public void onlyForBeforeLogin() {
-		System.out.println("onlyForBeforeLogin");
+	@Before(order=3)
+	public void before3() {
+		System.out.println("++ Starting before 3");
 	}
 	
-	@After(("@login"))
-	public void onlyForAfterLogin() {
-		System.out.println("onlyForAfterLogin");
 	
+	
+	
+	@After(order=1)
+	public void after1() {
+		System.out.println("++ Starting after 1");
 	}
 	
-	@BeforeStep(("@register"))
-	public void onlyForBeforeStepRegister() {
-		System.out.println("onlyForBeforeStepRegister");
+	@After(order=2)
+	public void after2() {
+		System.out.println("++ Starting after 2");
 	}
 	
-	@AfterStep(("@register"))
-	public void onlyForAfterStepRegister() {
-		System.out.println("onlyForAfterStepRegister");
-	
+	@After(order=3)
+	public void after3() {
+		System.out.println("++ Starting after 3");
 	}
-
+	
 }
