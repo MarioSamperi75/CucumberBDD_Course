ea409659a263daf7f5b214600ccbef6cc2bce79d
Mario Samperi <mariosamperi@Marios-MacBook-Pro.local>
2023-02-09 12:52:09 +0100

    using tag from the runner and from the console

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -8,12 +8,19 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 // you can specify a single feature to run
 // e.g. features = "src/test/java/features/Search.feature",
+
+// you can override the tag from the console
+// e.g. from source directory
+// mvn test -Dcucumber.filter.tags="@search"
+
+
 @CucumberOptions(
 		features = "src/test/resources/features",
 		glue = "stepdefinitions",
+		tags = "@smoke",
 		plugin = {"pretty","html:target/CucumberHTMLReport.html"}/*, tags="@login"*/
 		)
-public class __Runner {
+public class TestRunner {
 
 }
 
