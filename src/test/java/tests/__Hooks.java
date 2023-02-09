2c665f27bf43d0908eae789985ec9b4bfd613c92
Mario Samperi <mariosamperi@Marios-MacBook-Pro.local>
2023-02-09 09:21:45 +0100

    attribute value for the tag

--- src/test/java/tests/__Hooks.java
+++ src/test/java/tests/__Hooks.java
@@ -9,61 +9,40 @@ public class __Hooks {
 	
 	
 	
+	//The tag needs attribute "value" in case of multiple attributes
+	//e.g. 	@Before(order=1, value="@search")
+	@Before("@search")
+	public void setup() {
+		System.out.println("++ before hook - Setup method got executed ++");
+	}
+
+	@After("@search")
+	public void tearDown() {
+		System.out.println("-- before hook - TearDown method got executed --");
+
+	}
+
 	/*
-	 * @Before public void setup() { System.out.println("Browser got opened"); }
+	 * @Before(order=1) public void before1() {
+	 * System.out.println("++ Starting before 1"); }
 	 * 
-	 * @After public void tearDown() { System.out.println("Browser got closed");
+	 * @Before(order=2) public void before2() {
+	 * System.out.println("++ Starting before 2"); }
 	 * 
-	 * }
+	 * @Before(order=3) public void before3() {
+	 * System.out.println("++ Starting before 3"); }
 	 * 
-	 * @Before(("@login")) public void onlyForBeforeLogin() {
-	 * System.out.println("onlyForBeforeLogin"); }
 	 * 
-	 * @After(("@login")) public void onlyForAfterLogin() {
-	 * System.out.println("onlyForAfterLogin");
 	 * 
-	 * }
 	 * 
-	 * @BeforeStep(("@register")) public void onlyForBeforeStepRegister() {
-	 * System.out.println("onlyForBeforeStepRegister"); }
+	 * @After(order=1) public void after1() {
+	 * System.out.println("++ Starting after 1"); }
 	 * 
-	 * @AfterStep(("@register")) public void onlyForAfterStepRegister() {
-	 * System.out.println("onlyForAfterStepRegister"); }
+	 * @After(order=2) public void after2() {
+	 * System.out.println("++ Starting after 2"); }
+	 * 
+	 * @After(order=3) public void after3() {
+	 * System.out.println("++ Starting after 3"); }
 	 */
-	
-	
-	
-	@Before(order=1)
-	public void before1() {
-		System.out.println("++ Starting before 1");
-	}
-	
-	@Before(order=2)
-	public void before2() {
-		System.out.println("++ Starting before 2");
-	}
-	
-	@Before(order=3)
-	public void before3() {
-		System.out.println("++ Starting before 3");
-	}
-	
-	
-	
-	
-	@After(order=1)
-	public void after1() {
-		System.out.println("++ Starting after 1");
-	}
-	
-	@After(order=2)
-	public void after2() {
-		System.out.println("++ Starting after 2");
-	}
-	
-	@After(order=3)
-	public void after3() {
-		System.out.println("++ Starting after 3");
-	}
 	
 }
